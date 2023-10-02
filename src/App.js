5e87429b60974ad2cad2d3e0bf4a4f36337577f2
Gonzalo Borderolle <gborderolle@gmail.com>
2023-10-02 01:29:09 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import { useSelector } from 'react-redux';
+
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 
 function App() {
+  const isHidden = useSelector((state) => state.cart.isHidden);
+
   return (
     <Layout>
-      <Cart />
+      {!isHidden && <Cart />}
       <Products />
     </Layout>
   );
--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,8 +1,18 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { cartActions } from '../../store/cart';
+
 import classes from './CartButton.module.css';
 
 const CartButton = (props) => {
+  const dispatch = useDispatch();
+  const logout = useSelector((state) => state.logout);
+
+  const toggleHandler = () => {
+    dispatch(cartActions.toogleCart());
+  };
+
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={toggleHandler}>
       <span>My Cart</span>
       <span className={classes.badge}>1</span>
     </button>
